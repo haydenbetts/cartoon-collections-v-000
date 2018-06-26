dd309c0d8eaffb59774d374e07affe1f3a66d615
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 16:49:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |planet|
+    planet.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
